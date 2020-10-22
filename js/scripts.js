6a093f35ca5c7c63154cb14e94e25a2ab288ace7
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-22 13:19:51 -0700

    First version of Amusement Park >> Co-authored-by: Nick Hennessy <njhnny@gmail.com> Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  const height = parseInt(prompt("Please, enter your height in inches here:"));
+  //const weight = parseInt(prompt("Please, enter your weight in pounds here:"));
+
+
+  if (height <=48) {
+    $("#kids_rides").show();
+  } else if (height>48 && height<60) {
+    $("#kids_rides").show();
+    $("#adults_rides").show();
+  } else if (height>60) {
+      $("#adults_rides").show();
+  };
+
+});
